2b9c589216df25649450456889f01ec29d7bacc7
frastsytt <frastsytt@gmail.com>
2022-02-13 20:25:39 +0200

    API request through form

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const fetch = require('node-fetch')
 const path = require('path')
+const bodyParser = require('body-parser')
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
@@ -17,10 +19,25 @@ app.get('/', function(req, res) {
 		let description = data.weather[0].description
 		let city = data.name
 		let temp = Math.round(parseFloat(data.main.temp)-273.15)
-		console.log(description);
-		console.log(city);
-		console.log(temp);
-		res.render('default', {
+		res.render('index', {
+		description: description,
+		city: city,
+		temp: temp
+		})
+	})
+})
+
+app.post('/', urlencodedParser, function(req, res){
+	let city = req.body.cityname
+	fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`)
+	.then((response) => {
+		return response.json()
+	})
+	.then((data) => {
+		let description = data.weather[0].description
+		let city = data.name
+		let temp = Math.round(parseFloat(data.main.temp)-273.15)
+		res.render('index', {
 		description: description,
 		city: city,
 		temp: temp
